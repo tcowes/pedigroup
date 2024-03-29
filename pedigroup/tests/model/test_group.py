e4e5b92a3a0ae65f9772082d49c4c10f7509d671
l_ziege <lucasziegemann@gmail.com>
2024-03-29 04:58:43 -0300

    Modifico nombre a una variable para mantener un mismo idioma en las mismas

--- pedigroup/tests/model/test_group.py
+++ pedigroup/tests/model/test_group.py
@@ -8,25 +8,25 @@ class TestGroup(unittest.TestCase):
 
     def setUp(self):
         self.group = Group("Epersonal")
-        self.usuario = User("Lucas", "Ziegemann", "lziege", 1147454554, 1)
+        self.user = User("Lucas", "Ziegemann", "lziege", 1147454554, 1)
     
     def test_un_grupo_es_creado_sin_usuarios_y_sin_pedidos(self):
         self.assertEqual(self.group.usersQuantity(), 0)
         self.assertEqual(self.group.ordersQuantity(), 0)
 
     def test_un_grupo_tiene_un_usuario_luego_de_añadirsele_uno(self):
-        self.group.addUser(self.usuario)
+        self.group.addUser(self.user)
         self.assertEqual(self.group.usersQuantity(), 1)
 
     def test_un_grupo_no_tiene_usuarios_luego_de_eliminar_uno_que_fue_añadido_previamente(self):
-        self.group.addUser(self.usuario)
+        self.group.addUser(self.user)
         self.assertEqual(self.group.usersQuantity(), 1)
-        self.group.removeUser(self.usuario)
+        self.group.removeUser(self.user)
         self.assertEqual(self.group.usersQuantity(), 0)
 
     def test_al_intentar_eliminar_un_usuario_de_un_grupo_al_cual_no_pertenecia_se_lanza_excepcion(self):
         with self.assertRaises(CannotBeRemovedException):
-            self.group.removeUser(self.usuario)
+            self.group.removeUser(self.user)
 
     def test_un_grupo_tiene_un_pedido_luego_de_añadirsele_uno(self):
         order = Order(self.group)
--- pedigroup/tests/model/test_user.py
+++ pedigroup/tests/model/test_user.py
@@ -6,25 +6,25 @@ from model.exceptions.cannotBeRemovedException import CannotBeRemovedException
 class TestUser(unittest.TestCase):
 
     def setUp(self):
-        self.usuario = User("Lucas", "Ziegemann", "lziege", 1147454554, 1)
+        self.user = User("Lucas", "Ziegemann", "lziege", 1147454554, 1)
         self.group = Group("Epersonal")
     
     def test_un_usuario_es_creado_sin_grupos(self):
-        self.assertEqual(self.usuario.groupsQuantity(), 0)
+        self.assertEqual(self.user.groupsQuantity(), 0)
 
     def test_un_usuario_tiene_un_grupo_luego_de_añadirsele_uno(self):
-        self.usuario.addGroup(self.group)
-        self.assertEqual(self.usuario.groupsQuantity(), 1)
+        self.user.addGroup(self.group)
+        self.assertEqual(self.user.groupsQuantity(), 1)
 
     def test_un_usuario_no_tiene_grupos_luego_de_eliminar_uno_al_que_fue_añadido_previamente(self):
-        self.usuario.addGroup(self.group)
-        self.assertEqual(self.usuario.groupsQuantity(), 1)
-        self.usuario.removeGroup(self.group)
-        self.assertEqual(self.usuario.groupsQuantity(), 0)
+        self.user.addGroup(self.group)
+        self.assertEqual(self.user.groupsQuantity(), 1)
+        self.user.removeGroup(self.group)
+        self.assertEqual(self.user.groupsQuantity(), 0)
 
     def test_al_intentar_eliminar_un_grupo_de_un_usuario_al_cual_no_pertenecia_se_lanza_excepcion(self):
         with self.assertRaises(CannotBeRemovedException):
-            self.usuario.removeGroup(self.group)
+            self.user.removeGroup(self.group)
     
 if __name__ == '__main__':
     unittest.main()
