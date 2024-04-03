924ecb9016718afc332839d4333c1f00e6e62ef3
tcowes <tomascowes97@gmail.com>
2024-04-03 00:57:28 -0300

    Agregamos funcionalidades basicas del bot

--- pedigroup/telegram_integration/bot.py
+++ pedigroup/telegram_integration/bot.py
@@ -1,13 +1,16 @@
+from collections import defaultdict
+from typing import Dict
 import environ
 env = environ.Env()
 environ.Env.read_env()
 
 import logging
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram import Chat, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
     ContextTypes,
+    CallbackQueryHandler,
     ConversationHandler,
     MessageHandler,
     filters,
@@ -15,148 +18,115 @@ from telegram.ext import (
 
 BOT_TOKEN = env("TELEGRAM_BOT_TOKEN")
 
-# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+# Variable global para trackear el estado de un pedido.
+order_started = False
+# Variable global para ir almacenando el pedido
+current_order: Dict[int, Dict[str, int]] = defaultdict(lambda: defaultdict(lambda: 0))
+
+TYPE_SELECTION, QUANTITY = range(2)
 
 
-async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+async def handle_order(update: Update, context: ContextTypes.DEFAULT_TYPE, starting_order: bool):
+    if update.message.chat.type == Chat.PRIVATE:
+        await update.message.reply_text("Este comando solo puede llamarse desde un grupo.")
+        return
 
-
-async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
-
-
-GENDER, PHOTO, LOCATION, BIO = range(4)
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Starts the conversation and asks the user about their gender."""
-    reply_keyboard = [["Boy", "Girl", "Other"]]
-
-    await update.message.reply_text(
-        "Hi! My name is Professor Bot. I will hold a conversation with you. "
-        "Send /cancel to stop talking to me.\n\n"
-        "Are you a boy or a girl?",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
-        ),
-    )
-
-    return GENDER
-
-
-async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the selected gender and asks for a photo."""
     user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text(
-        "I see! Please send me a photo of yourself, "
-        "so I know what you look like, or send /skip if you don't want to.",
-        reply_markup=ReplyKeyboardRemove(),
-    )
+    group_id = update.message.chat_id
+    
+    global order_started
+    global current_order
+    match starting_order, order_started:
+        case True, False:
+            message = f"{user.first_name} inició un pedido de empanadas, quienes quieran pedir deben enviar su pedido."
+            order_started = True
+        case True, True:
+            message = "Ya hay un pedido en curso, finalizar con /finalizar_pedido"
+        case False, True:
+            # TODO: formatear correctamente el current_order
+            message = f"{user.first_name} finalizó el pedido! {current_order}"
+            current_order = {}
+            order_started = False
+        case False, False:
+            message = "No hay ningún pedido en curso, iniciar uno nuevo con /iniciar_pedido"
 
-    return PHOTO
-
-
-async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the photo and asks for a location."""
-    user = update.message.from_user
-    photo_file = await update.message.photo[-1].get_file()
-    await photo_file.download_to_drive("user_photo.jpg")
-    logger.info("Photo of %s: %s", user.first_name, "user_photo.jpg")
-    await update.message.reply_text(
-        "Gorgeous! Now, send me your location please, or send /skip if you don't want to."
-    )
-
-    return LOCATION
+    await context.bot.send_message(group_id, message)
 
 
-async def skip_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Skips the photo and asks for a location."""
-    user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    await update.message.reply_text(
-        "I bet you look great! Now, send me your location please, or send /skip."
-    )
-
-    return LOCATION
+async def start_order(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await handle_order(update, context, starting_order=True)
 
 
-async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the location and asks for some info about the user."""
-    user = update.message.from_user
-    user_location = update.message.location
-    logger.info(
-        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
-    )
-    await update.message.reply_text(
-        "Maybe I can visit you sometime! At last, tell me something about yourself."
-    )
-
-    return BIO
+async def finish_order(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await handle_order(update, context, starting_order=False)
 
 
-async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Skips the location and asks for info about the user."""
-    user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
-    await update.message.reply_text(
-        "You seem a bit paranoid! At last, tell me something about yourself."
-    )
+async def show_empanada_variety(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # TODO: falta handleo de si está o no el pedido inciado
+    if update.message.chat.type != Chat.PRIVATE:
+        await update.message.reply_text("Este comando solo puede llamarse desde un chat privado.")
+        return
+    
+    empanada_variety = ["Jamón y Queso", "Carne", "Verdura"]  # TODO: traer de la DB
+    keyboard = []
+    for empanada_type in empanada_variety:
+        keyboard.append([InlineKeyboardButton(empanada_type, callback_data=empanada_type)])
 
-    return BIO
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Selecciona qué gusto de empanada te gustaría pedir:", reply_markup=reply_markup)
+
+    return TYPE_SELECTION
 
 
-async def bio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the info about the user and ends the conversation."""
-    user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text("Thank you! I hope we can talk again some day.")
+async def handle_empanada_selection(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # TODO: falta handleo de si está o no el pedido inciado
+    query = update.callback_query
+    empanada_type = query.data
+    await query.answer()
+
+    context.user_data['empanada_type'] = empanada_type
+    await query.message.reply_text(f"Seleccionaste {empanada_type}. ¿Cuantas quisieras pedir?")
+
+    return QUANTITY
+
+
+async def handle_empanada_quantity(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # TODO: falta handleo de si está o no el pedido inciado
+    # TODO: asegurar que nos manden un int.
+    quantity = int(update.message.text)
+
+    context.user_data['quantity'] = quantity
+    empanada_type = context.user_data['empanada_type']
+    await update.message.reply_text(f"Has seleccionado {quantity} empanadas de tipo {empanada_type}.")
+
+    current_order[update.message.from_user.id][empanada_type] += quantity
 
     return ConversationHandler.END
-
-
-async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Cancels and ends the conversation."""
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    await update.message.reply_text(
-        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
-    )
-
-    return ConversationHandler.END
-
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     
-    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
+    application.add_handler(CommandHandler("iniciar_pedido", start_order))
+    application.add_handler(CommandHandler("finalizar_pedido", finish_order))
+
+    individual_order_handler = ConversationHandler(
+        entry_points=[CommandHandler("empanadas", show_empanada_variety)],
         states={
-            GENDER: [MessageHandler(filters.Regex("^(Boy|Girl|Other)$"), gender)],
-            PHOTO: [MessageHandler(filters.PHOTO, photo), CommandHandler("skip", skip_photo)],
-            LOCATION: [
-                MessageHandler(filters.LOCATION, location),
-                CommandHandler("skip", skip_location),
-            ],
-            BIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, bio)],
+            TYPE_SELECTION: [CallbackQueryHandler(handle_empanada_selection)],
+            QUANTITY: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_empanada_quantity)],
         },
-        fallbacks=[CommandHandler("cancel", cancel)],
+        fallbacks=[],
     )
 
-    application.add_handler(conv_handler)
-    
+    application.add_handler(individual_order_handler)
+
+
     application.run_polling()
