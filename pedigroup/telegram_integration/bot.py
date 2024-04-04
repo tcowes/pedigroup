ba5f8bd6d31a5e514ff72fd295b11cbd4c8f6807
tcowes <tomascowes97@gmail.com>
2024-04-04 01:23:56 -0300

    Terminamos el flujo del bot

--- pedigroup/telegram_integration/bot.py
+++ pedigroup/telegram_integration/bot.py
@@ -33,6 +33,12 @@ current_order: Dict[int, Dict[str, int]] = defaultdict(lambda: defaultdict(lambd
 
 TYPE_SELECTION, QUANTITY = range(2)
 
+def format_order(total_order: dict) -> str:
+    grouped_order = defaultdict(lambda: 0)
+    for _, order in total_order.items():
+        for item, quantity in order.items():
+            grouped_order[item] += quantity
+    return "\n".join([f"{item}: {quantity}" for item, quantity in grouped_order.items()])
 
 async def handle_order(update: Update, context: ContextTypes.DEFAULT_TYPE, starting_order: bool):
     if update.message.chat.type == Chat.PRIVATE:
@@ -42,17 +48,23 @@ async def handle_order(update: Update, context: ContextTypes.DEFAULT_TYPE, start
     user = update.message.from_user
     group_id = update.message.chat_id
     
+    logger.debug(f"{user.first_name} ({user.id}) called {'/iniciar_pedido' if starting_order else '/finalizar_pedido'}")
+
     global order_started
     global current_order
     match starting_order, order_started:
         case True, False:
-            message = f"{user.first_name} inició un pedido de empanadas, quienes quieran pedir deben enviar su pedido."
+            message = (
+                f"{user.first_name} inició un pedido de empanadas!"
+                "\n\nQuienes quieran pedir deben contactarse conmigo mediante un chat privado "
+                "con el comando /pedido_individual."
+            )
             order_started = True
         case True, True:
             message = "Ya hay un pedido en curso, finalizar con /finalizar_pedido"
         case False, True:
-            # TODO: formatear correctamente el current_order
-            message = f"{user.first_name} finalizó el pedido! {current_order}"
+            formatted_order = format_order(current_order)
+            message = f"{user.first_name} finalizó el pedido!\n\nEn total se pidieron:\n{formatted_order}"
             current_order = {}
             order_started = False
         case False, False:
@@ -69,45 +81,52 @@ async def finish_order(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await handle_order(update, context, starting_order=False)
 
 
-async def show_empanada_variety(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # TODO: falta handleo de si está o no el pedido inciado
+async def show_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global order_started
+    if not order_started:
+        await update.message.reply_text("Este comando solo puede llamarse una vez que alguien haya iniciado un pedido en un grupo con /iniciar_pedido.")
+        return
     if update.message.chat.type != Chat.PRIVATE:
         await update.message.reply_text("Este comando solo puede llamarse desde un chat privado.")
         return
     
-    empanada_variety = ["Jamón y Queso", "Carne", "Verdura"]  # TODO: traer de la DB
+    empanada_variety = ["Jamón y Queso", "Carne", "Verdura", "Pollo"]  # TODO: traer de la DB
     keyboard = []
     for empanada_type in empanada_variety:
         keyboard.append([InlineKeyboardButton(empanada_type, callback_data=empanada_type)])
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Selecciona qué gusto de empanada te gustaría pedir:", reply_markup=reply_markup)
+    await update.message.reply_text("Seleccioná que gusto de empanada te gustaría pedir:", reply_markup=reply_markup)
 
     return TYPE_SELECTION
 
 
-async def handle_empanada_selection(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # TODO: falta handleo de si está o no el pedido inciado
+async def handle_type_selection(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     empanada_type = query.data
     await query.answer()
 
     context.user_data['empanada_type'] = empanada_type
-    await query.message.reply_text(f"Seleccionaste {empanada_type}. ¿Cuantas quisieras pedir?")
+    await query.message.reply_text(f"¿Cuantas empanadas de {empanada_type} quisieras pedir? Escribí solamente el número:")
 
     return QUANTITY
 
 
-async def handle_empanada_quantity(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # TODO: falta handleo de si está o no el pedido inciado
-    # TODO: asegurar que nos manden un int.
+async def handle_quantity(update: Update, context: ContextTypes.DEFAULT_TYPE):
     quantity = int(update.message.text)
 
     context.user_data['quantity'] = quantity
     empanada_type = context.user_data['empanada_type']
-    await update.message.reply_text(f"Has seleccionado {quantity} empanadas de tipo {empanada_type}.")
+    user = update.message.from_user
 
-    current_order[update.message.from_user.id][empanada_type] += quantity
+    await update.message.reply_text(
+        f"Agregué {quantity} de {empanada_type} al pedido grupal!"
+        "\n\nSi querés seguir agregando empanadas podes volver a pedir con /pedido_individual."
+        "\nSi nadie mas quiere agregar pedidos, pueden finalizar el pedido desde el grupo con /finalizar_pedido."
+    )
+
+    logger.debug(f"{user.first_name} ({user.id}) added {quantity} {empanada_type}")
+    current_order[user.id][empanada_type] += quantity
 
     return ConversationHandler.END
 
@@ -118,15 +137,14 @@ if __name__ == '__main__':
     application.add_handler(CommandHandler("finalizar_pedido", finish_order))
 
     individual_order_handler = ConversationHandler(
-        entry_points=[CommandHandler("empanadas", show_empanada_variety)],
+        entry_points=[CommandHandler("pedido_individual", show_menu)],
         states={
-            TYPE_SELECTION: [CallbackQueryHandler(handle_empanada_selection)],
-            QUANTITY: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_empanada_quantity)],
+            TYPE_SELECTION: [CallbackQueryHandler(handle_type_selection)],
+            QUANTITY: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_quantity)],
         },
         fallbacks=[],
     )
 
     application.add_handler(individual_order_handler)
-
 
     application.run_polling()
