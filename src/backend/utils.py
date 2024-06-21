f05c009c435d1085bfc58f6e49a59c6028028106
l_ziege <lucasziegemann@gmail.com>
2024-06-20 23:03:40 -0300

    Feature de visualizacion de los pedidos individuales que un usuario hizo a un grupo añadida

--- src/backend/utils.py
+++ src/backend/utils.py
@@ -3,7 +3,6 @@ from typing import Tuple
 
 from backend.models import Order, GroupOrder
 
-
 def format_order(orders: list[Order]) -> Tuple[str, int]:
     grouped_order = defaultdict(lambda: 0)
     total_quantity = 0
@@ -12,6 +11,15 @@ def format_order(orders: list[Order]) -> Tuple[str, int]:
         total_quantity += order.quantity
     return "\n".join([f"\t • {prod_name}: {n}" for prod_name, n in grouped_order.items()]), total_quantity
 
+def format_individual_orders(orders: list[Order]) -> Tuple[str, int, int]:
+    grouped_orders = defaultdict(lambda: 0)
+    total_quantity = 0
+    estimated_price = 0
+    for order in orders:
+        grouped_orders[order.product_name()] += order.quantity
+        total_quantity += order.quantity
+        estimated_price += order.estimated_price
+    return "\n".join([f"\t • {prod_name}: {n}" for prod_name, n in grouped_orders.items()]), total_quantity, estimated_price
 
 def format_group_orders_with_date(orders: list[GroupOrder], **filter_criteria) -> str:
     """Precondición: el listado de pedidos debe venir con al menos un elemento, y ordenado por fecha de pedido
